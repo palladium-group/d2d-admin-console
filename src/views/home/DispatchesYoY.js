c9e3238065dda5766309d0f6dd95b343da7cab60
felixkithinji <felixkithinji@outlook.com>
2024-05-24 11:37:32 +0300

    load YoY chart after data has come

--- src/views/home/DispatchesYoY.js
+++ src/views/home/DispatchesYoY.js
@@ -1,10 +1,39 @@
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import { useEffect, useState } from 'react';
+import { Box, CircularProgress } from '@mui/material';
 
 const DispatchesYoY = ({ height, data }) => {
   const [options, setOptions] = useState({});
-  console.log(data.facilitiesProcessedByYear);
+  const [series, setSeries] = useState([]);
+  const currentMonth = new Date().getMonth();
+  const currentYear = new Date().getFullYear();
+
+  useEffect(() => {
+    if (
+      data &&
+      data.facilitiesProcessedByYear &&
+      data.facilitiesProcessedByYear.length > 0
+    ) {
+      const seriesData = [];
+      for (const yearProcessed of data.facilitiesProcessedByYear) {
+        const yearData = { name: yearProcessed.name, data: [] };
+        for (let i = 1; i <= 12; i++) {
+          if (yearProcessed.name == currentYear && i === currentMonth + 2) {
+            break;
+          }
+          const val = yearProcessed.data.find((obj) => obj.month === i);
+          if (val) {
+            yearData.data.push(val.sum);
+          } else {
+            yearData.data.push(0);
+          }
+        }
+        seriesData.push(yearData);
+      }
+      setSeries(seriesData);
+    }
+  }, [data]);
 
   useEffect(() => {
     setOptions({
@@ -15,13 +44,11 @@ const DispatchesYoY = ({ height, data }) => {
         text: 'Number of Dispatches Processed YoY',
         align: 'left'
       },
-
       yAxis: {
         title: {
           text: 'Number of Dispatches'
         }
       },
-
       xAxis: [
         {
           accessibility: {
@@ -57,13 +84,12 @@ const DispatchesYoY = ({ height, data }) => {
           }
         }
       },
-
-      series: data.facilitiesProcessedByYear,
+      series: series,
       responsive: {
         rules: [
           {
             condition: {
-              maxWidth: 500
+              // maxHeight: height
             },
             chartOptions: {
               legend: {
@@ -76,7 +102,13 @@ const DispatchesYoY = ({ height, data }) => {
         ]
       }
     });
-  }, []);
-  return <HighchartsReact highcharts={Highcharts} options={options} />;
+  }, [series, height]);
+  return series.length === 0 ? (
+    <Box display="flex" justifyContent="center" my={6}>
+      <CircularProgress />
+    </Box>
+  ) : (
+    <HighchartsReact highcharts={Highcharts} options={options} />
+  );
 };
 export default DispatchesYoY;
