c41288c35945717f3d5dbb59ff6670801826c4ed
Christopher Githu <christopher.githu@gmail.com>
2024-06-03 17:32:36 +0300

    increase recency cards width

--- src/views/home/RunDeckInfo.js
+++ src/views/home/RunDeckInfo.js
@@ -71,7 +71,7 @@ const RunDeckInfo = () => {
         <Grid container spacing={gridSpacing} style={{ flexGrow: 1 }}>
           <Grid
             item
-            md={3}
+            md={4}
             style={{ display: 'flex', flexDirection: 'column' }}
             ref={leftColumnRef}
           >
@@ -204,7 +204,7 @@ const RunDeckInfo = () => {
           </Grid>
           <Grid
             item
-            md={9}
+            md={8}
             style={{
               display: 'flex',
               flexDirection: 'column',
