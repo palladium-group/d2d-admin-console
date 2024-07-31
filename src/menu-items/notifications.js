646f5ca6bd0bd52a90c965763554a7d6a4626d08
Christopher Githu <christopher.githu@gmail.com>
2024-07-31 11:37:45 +0000

    Notifications Menu Item

---
+++ src/menu-items/notifications.js
@@ -0,0 +1,11 @@
+import { IconBell } from '@tabler/icons';
+
+const notifications = {
+  id: 'notifications',
+  title: 'Notifications',
+  type: 'group',
+  icon: IconBell,
+  url: '/notifications'
+};
+
+export default notifications;
